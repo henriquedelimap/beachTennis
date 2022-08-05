f8f054dafa5c47b6b49a19fc91b02e283354956a
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-05 12:46:25 -0300

    aa

--- src/components/FloatingButton/index.tsx
+++ src/components/FloatingButton/index.tsx
@@ -1,6 +1,8 @@
-import { Fab, Stack, Typography } from '@mui/material'
+import { Fab, Link, Stack, Typography } from '@mui/material'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 export const FloatingButton = () => {
+    const navigate = useNavigate()
     return (
         <Fab
             variant='extended'
@@ -11,12 +13,16 @@ export const FloatingButton = () => {
                 bottom: 16,
                 right: 16
             }}
+            href='https://wa.me/message/HX63ODKP56H5L1'
         >
+
             <Stack direction='row' spacing={1} alignItems='center'>
+                
                 <BsWhatsapp fontSize={20} />
                 <Typography variant={'body1'}>
                     entrar em contato
                 </Typography>
+                
             </Stack>
         </Fab>
     )
