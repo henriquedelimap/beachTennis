d9d4637c29bf60f79e1700668d3b2daf61b1603b
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-05 20:36:07 -0300

    first commit

--- src/components/Sale/index.tsx
+++ src/components/Sale/index.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled"
-import { Box, Button, Grid, Stack } from "@mui/material"
+import { Box, Button, Grid, Stack, Typography } from "@mui/material"
 import { MdBeachAccess, MdShop, MdShop2, MdShoppingCart, MdShopTwo } from "react-icons/md"
 import { semFundo1, semFundo2, semFundo3 } from "../../assets/img"
 
@@ -9,33 +9,35 @@ export const Sale = () => {
     const promocoes = [
         {
             img: semFundo1,
-            colorRadial: 'radial-gradient(#ffffff, #ffffff, #B7EFAD, #61F248, #61F248, #61F248)',
-            color: '#61F248'
+            colorRadial: 'radial-gradient(#ffffff, #ffffff, #FFFAD2, #FFEA38, #FFEA38, #FFEA38)',
+            color: '#FFEA38'
         },
-        
-        {
-            img: semFundo3,
-            colorRadial: 'radial-gradient( #ffffff, #ffffff, #F5B589, #F17F31, #F17F31, #F17F31)',
-            color: '#F17F31'
-            
-        },
+
         {
             img: semFundo2,
-            colorRadial: 'radial-gradient( #ffffff, #ffffff, #F5EB90, #EEDA29, #EEDA29, #EEDA29)',
-            color: '#EEDA29'
+            colorRadial: 'radial-gradient( #ffffff, #ffffff, #D3CFBE, #948753, #948753, #948753)',
+            color: '#948753'
+        },
+        {
+            img: semFundo3,
+            colorRadial: 'radial-gradient( #ffffff, #ffffff, #FFCDC3, #FE2B05, #FE2B05, #FE2B05)',
+            color: '#FE2B05'
+
         },
     ]
     return (
-        <Grid container spacing={2} justifyContent='center' alignContent='center' sx={{width: '100%'}} >
+        <Grid container spacing={2} justifyContent='center' alignContent='center' sx={{ width: '100%' }} >
             {
                 promocoes.map(item => (
-                    <Grid sx={{width: '100%'}} alignItems='center' justifyContent='center' item xs={3} md={3} lg={4}>
-                        <Stack alignItems='center' justifyContent='center' sx={{ position: 'relative', width: {lg: '100%' , md: '100%', xs: '100%'  }}}>
+                    <Grid sx={{ width: '100%' }} alignItems='center' justifyContent='center' item xs={3} md={3} lg={4}>
+                        <Stack alignItems='center' justifyContent='center' sx={{ position: 'relative', width: { lg: '100%', md: '100%', xs: '100%' } }}>
 
-                            <img src={item.img} style={{ zIndex: 3, height: 320  }} />
-                            <Fundo style={{background: item.colorRadial}} />
-                            <Button sx={{position:'absolute', color: '#666666', fontWeight: 'bold', zIndex: 5, background: item.color, bottom: '10%', whiteSpace: 'nowrap', p: 2, pt: .5, pb: .5}}>
-                                30% off
+                            <img src={item.img} style={{ zIndex: 3, height: 320 }} />
+                            <Fundo style={{ background: item.colorRadial }} />
+                            <Button sx={{ position: 'absolute', color: 'white', zIndex: 5, background: item.color, bottom: '12%', whiteSpace: 'nowrap', p: 2, pt: .5, pb: .5 }}>
+                                <Typography variant='body1' sx={{ textStroke: '2px red', fontWeight: 'bold' }}>
+                                    30% off
+                                </Typography>
                                 <Borda />
                                 <Borda2 />
                             </Button>
@@ -59,20 +61,20 @@ const Fundo = styled('div')(({ }) => ({
     zIndex: -1
 }))
 
-const Borda = styled('div')(({})=>({
+const Borda = styled('div')(({ }) => ({
     width: '100%',
     height: '100%',
-    border:'2px solid #666666',
+    border: '1px solid #111111',
     borderRadius: 6,
     position: 'absolute',
     zIndex: -1,
     top: -2,
     left: 2
 }))
-const Borda2 = styled('div')(({})=>({
+const Borda2 = styled('div')(({ }) => ({
     width: '100%',
     height: '100%',
-    border:'2px solid #666666',
+    border: '1px solid #111111',
     borderRadius: 6,
     position: 'absolute',
     zIndex: -1,
