fb8f9baf30a19f90cb4d1a571ebe28281a18096f
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-06 15:26:46 -0300

    first commit

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -8,13 +8,12 @@ import { fundoCarbono, fundoCarbono2, fundoCarbono3, logoHeros, X, Vector, Vecto
 import { motion, useScroll } from 'framer-motion'
 import { useRef } from "react"
 import { LogoQuicksand } from "../../assets/img/logoQuicksand"
-import { Overflow } from "../../styles"
 export const Home = () => {
     const { scrollYProgress } = useScroll()
     console.log(scrollYProgress);
 
     return (
-        <Overflow>
+        <>
 
             <ContainerPadding>
                 <Title text='promoção' />
@@ -34,34 +33,13 @@ export const Home = () => {
                         tecnologia de ponta
                     </Button>
                 </Stack>
-                <Vector2 index={2} top={10} left={'0%'} rotate={0} color={'white'} />
-                <Vector2 index={3} top={50} left={'0%'} rotate={0} color={'#D73F33'} />
-                <Vector2 index={2} top={90} left={'0%'} rotate={0} color={'green'} />
-
-                <Vector2 index={1} top={-125} left={'-38%'} rotate={90} color={'#0066cc'} />
-                <Vector2 index={2} top={-120} left={'-34%'} rotate={90} color={'#0066cc'} />
-
-                <Vector2 index={1} top={-125} left={'-30%'} rotate={90} color={'#0066cc'} />
-                <Vector2 index={2} top={-200} left={'-26%'} rotate={90} color={'#0066cc'} />
-                <Vector2 index={3} top={-225} left={'16%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={2} top={-250} left={'20%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-275} left={'24%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-300} left={'28%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-300} left={'28%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-225} left={'48%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-200} left={'44%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-175} left={'40%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-150} left={'36%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-125} left={'32%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-250} left={'52%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={-275} left={'58%'} rotate={-90} color={'#0066cc'} />
-
+                
                 
             </Stack>
             <Stack sx={{ height: 1000 }}>
 
             </Stack>
-        </Overflow>
+        </>
     )
 }
 
---
+++ src/components/Animation/index.tsx
@@ -0,0 +1,30 @@
+import { Vector2 } from "../../assets/img"
+
+export const VectorBackground = () => {
+    return (
+        <>
+            <Vector2 index={2} top={10} left={'0%'} rotate={0} color={'white'} />
+            <Vector2 index={3} top={50} left={'0%'} rotate={0} color={'#D73F33'} />
+            <Vector2 index={2} top={90} left={'0%'} rotate={0} color={'green'} />
+
+            <Vector2 index={1} top={-125} left={'-38%'} rotate={90} color={'#0066cc'} />
+            <Vector2 index={2} top={-120} left={'-34%'} rotate={90} color={'#0066cc'} />
+
+            <Vector2 index={1} top={-125} left={'-30%'} rotate={90} color={'#0066cc'} />
+            <Vector2 index={2} top={-200} left={'-26%'} rotate={90} color={'#0066cc'} />
+            <Vector2 index={3} top={-225} left={'16%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={2} top={-250} left={'20%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-275} left={'24%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-300} left={'28%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-300} left={'28%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-225} left={'48%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-200} left={'44%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-175} left={'40%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-150} left={'36%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-125} left={'32%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-250} left={'52%'} rotate={-90} color={'#0066cc'} />
+            <Vector2 index={1} top={-275} left={'58%'} rotate={-90} color={'#0066cc'} />
+
+        </>
+    )
+}
