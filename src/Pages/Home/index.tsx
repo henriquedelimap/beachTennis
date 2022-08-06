85e54830ea2416802fc3f1e68de2072497beab55
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-06 13:25:37 -0300

    first commit

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -34,23 +34,23 @@ export const Home = () => {
                         tecnologia de ponta
                     </Button>
                 </Stack>
-                <Vector2 index={1} top={-10} left={'-10%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={55} left={'-14%'} rotate={-90} color={'#ffffff'} />
-                <Vector2 index={1} top={125} left={'-18%'} rotate={-90} color={'#D73F33'} />
-                <Vector2 index={1} top={190} left={'-22%'} rotate={-90} color={'#0066cc'} />
+                <Vector2 index={2} top={-10} left={'-10%'} rotate={-90} color={'#0066cc'} />
+                <Vector2 index={2} top={55} left={'-14%'} rotate={-90} color={'#ffffff'} />
+                <Vector2 index={2} top={125} left={'-18%'} rotate={-90} color={'#D73F33'} />
+                <Vector2 index={2} top={190} left={'-22%'} rotate={-90} color={'#0066cc'} />
 
-                <Vector2 index={-1} top={-55} left={'-5%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={-1} top={-125} left={'0%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={-1} top={-190} left={'5%'} rotate={-90} color={'#0066cc'} />
+                <Vector2 index={1} top={-55} left={'-5%'} rotate={-90} color={'#0066cc'} />
+                <Vector2 index={1} top={-125} left={'0%'} rotate={-90} color={'#0066cc'} />
+                <Vector2 index={1} top={-190} left={'5%'} rotate={-90} color={'#0066cc'} />
 
-                <Vector2 index={1} top={-10} right={'-10%'} rotate={-90} color={'#D73F33'} />
-                <Vector2 index={1} top={55} right={'-14%'} rotate={-90} color={'#ffffff'} />
-                <Vector2 index={1} top={125} right={'-18%'} rotate={-90} color={'#0066cc'} />
-                <Vector2 index={1} top={190} right={'-22%'} rotate={-90} color={'#D73F33'} />
+                <Vector2 index={2} top={-10} right={'-10%'} rotate={-90} color={'#D73F33'} />
+                <Vector2 index={2} top={55} right={'-14%'} rotate={-90} color={'#ffffff'} />
+                <Vector2 index={2} top={125} right={'-18%'} rotate={-90} color={'#0066cc'} />
+                <Vector2 index={2} top={190} right={'-22%'} rotate={-90} color={'#D73F33'} />
 
-                <Vector2 index={-1} top={-55} right={'-5%'} rotate={90} color={'#D73F33'} />
-                <Vector2 index={-1} top={-125} right={'0%'} rotate={90} color={'#D73F33'} />
-                <Vector2 index={-1} top={-190} right={'5%'} rotate={90} color={'#D73F33'} />
+                <Vector2 index={1} top={-55} right={'-5%'} rotate={90} color={'#D73F33'} />
+                <Vector2 index={1} top={-125} right={'0%'} rotate={90} color={'#D73F33'} />
+                <Vector2 index={1} top={-190} right={'5%'} rotate={90} color={'#D73F33'} />
             </Stack>
             <Stack sx={{ height: 1000 }}>
 
