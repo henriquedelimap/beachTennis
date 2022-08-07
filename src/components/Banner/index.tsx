7bf321aaa763c416276afd54e2539d6cb694690f
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-07 18:57:52 -0300

    first commit

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,6 +4,7 @@ import Carousel from 'react-material-ui-carousel'
 import { Fundo } from "../../styles"
 import { banner1, banner2, banner3, banner4 } from "../../assets/img"
 
+import {AppearEffect} from '../Animation'
 export const Banner = () => {
     const images = [banner1, banner2, banner3, banner4]
     return (
@@ -47,7 +48,7 @@ export const Banner = () => {
     )
 }
 
-interface Prop{
+interface Prop {
 
     logo?: any
     description: string
@@ -62,29 +63,62 @@ interface Prop{
 }
 
 export const BannerGlass = (prop: Prop) => {
-    const {logo, raquetes, description, btnText} = prop
+    const { logo, raquetes, description, btnText } = prop
     return (
         <Stack sx={{ overflow: 'hidden', position: 'relative', background: '#111217' }} direction='row' justifyContent='center' alignItems='center'>
-            <Stack spacing={2} sx={{ width: { lg: '32%', md: '64%', xs: '100%' }, height: '16rem', background: '#1f1f1faf', backdropFilter: 'blur(4px)', zIndex: 90 }} alignItems='center' justifyContent='center'>
-                <img src={logo} style={{ zIndex: 100, width: '12rem' }} />
-                <Typography sx={{ p: 4, pt: 0, pb: 4, textAlign: 'center' }} color='white' paragraph>
+            <Stack 
+                spacing={2} 
+                sx={{ 
+                    width: { 
+                        lg: '32%', 
+                        md: '64%', 
+                        xs: '100%' }, 
+                        height: '16rem', 
+                        background: '#1f1f1faf', backdropFilter: 'blur(4px)', zIndex: 90 }} 
+                        alignItems='center' justifyContent='center'
+                    >
+                <img 
+                    src={logo} 
+                    style={{ 
+                        zIndex: 100, 
+                        width: '12rem' 
+                }} />
+                <Typography 
+                    sx={{ 
+                        p: 4, 
+                        pt: 0, 
+                        pb: 4, 
+                        textAlign: 'center' 
+                        }} 
+                        color='white' 
+                        paragraph>
                     {description}
-                    
+
                 </Typography>
-                <Btn size={18} color={'#f1f1f11f'} text={btnText} />
+                <Btn 
+                    size={18} 
+                    color={'#f1f1f11f'} 
+                    text={btnText} />
             </Stack>
-            <Stack width='100%' height='100%' sx={{ position: 'absolute' }}>
+            <Stack 
+                width='100%' 
+                height='100%' 
+                sx={{ 
+                    position: 'absolute' }}>
 
 
                 {
                     raquetes?.map((raquete, index) => {
                         return (
 
-                            // <AppearEffect key={index} index={raquete.index} x={'50%'} xA={raquete.xA} y={0} yA={0} isImg={false} />
+                            <AppearEffect key={index} y={100} isImg={false} yA={0} rotate={-8}>
+
 
                             <Fundo key={index} right={raquete.xA} >
                                 <img src={raquete.img} style={{ height: '16rem', zIndex: index }} />
                             </Fundo>
+
+                            </AppearEffect>
                         )
                     })
                 }
