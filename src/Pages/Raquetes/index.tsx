7b0e8059ab0fa57a330fc48abe504bd14b3ed173
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-08 18:31:03 -0300

    first commit

--- src/Pages/Raquetes/index.tsx
+++ src/Pages/Raquetes/index.tsx
@@ -7,18 +7,46 @@ import { ContainerPadding } from "../../components/Container"
 export const RaquetesPage = () => {
     const height = useWindowDimensions().height
     return (
-       <ContainerPadding>
-                <Title text='raquetes' />
-                <Grid  container justifyContent='center' alignItems='cneter' rowSpacing={2} spacing={{lg: 2, md: 2, xs: 0}}>
+        <ContainerPadding>
+            <Title text='raquetes' />
+            <Grid
+                container
+                justifyContent='center'
+                alignItems='cneter'
+                rowSpacing={2}
+                spacing={
                     {
-                        Data.map((item, index) => (
-                            <Grid key={index} item container justifyContent='center' alignItems='center' xs={12} md={4} lg={3}  >
-                                <CardRaquetes animated={false} item={item} />
+                        lg: 2,
+                        md: 2,
+                        xs: 0
+                    }
+                }
+            >
+                {
+                    Data.map(
+                        (
+                            item,
+                            index
+                        ) => (
+                            <Grid
+                                key={index}
+                                item
+                                container
+                                justifyContent='center'
+                                alignItems='center'
+                                xs={12}
+                                md={4}
+                                lg={3}
+                            >
+                                <CardRaquetes
+                                    animated={false}
+                                    item={item}
+                                />
                             </Grid>
                         ))
 
-                    }
-                </Grid>
+                }
+            </Grid>
         </ContainerPadding>
     )
 }
