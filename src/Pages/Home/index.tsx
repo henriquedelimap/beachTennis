8995cd47f1bc003be79267b3c80f5dc1dc53807c
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-09 15:27:55 -0300

    first commit

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -19,8 +19,8 @@ import { useEffect, useState } from "react"
 export const Home = () => {
     const { scrollYProgress } = useScroll()
     const [itemComparative1, setItemComparative1] = useState('black death 10.2 gold 2022')
-    const [firstItemComparative, setFirstItemComparative] = useState<IRaquete >(Data.map(item=>item)[0])
-    const [secondItemComparative, setSecondItemComparative] = useState<IRaquete >(Data.map(item=>item)[1])
+    const [firstItemComparative, setFirstItemComparative] = useState<IRaquete>(Data.map(item => item)[0])
+    const [secondItemComparative, setSecondItemComparative] = useState<IRaquete>(Data.map(item => item)[1])
     const [itemComparative2, setItemComparative2] = useState('DNA EXTREME 2.2 ORANGE 2022')
     const raquetes = [
 
@@ -70,34 +70,38 @@ export const Home = () => {
 
 
     ]
-    let object1 = Data.map(item=> {if(item.title === itemComparative1) {
-        return item
-    }}).filter(i=>i)[0]
-    let object2 = Data.map(item=> {if(item.title === itemComparative2) {
-        return item
-    }}).filter(i=>i)[0]
-    
+    let object1 = Data.map(item => {
+        if (item.title === itemComparative1) {
+            return item
+        }
+    }).filter(i => i)[0]
+    let object2 = Data.map(item => {
+        if (item.title === itemComparative2) {
+            return item
+        }
+    }).filter(i => i)[0]
+
     useEffect(() => {
 
-        if(object1 === undefined || object2 === undefined) {
-            setFirstItemComparative(Data.map(item=>item)[0])
-            setSecondItemComparative(Data.map(item=>item)[1])
+        if (object1 === undefined || object2 === undefined) {
+            setFirstItemComparative(Data.map(item => item)[0])
+            setSecondItemComparative(Data.map(item => item)[1])
         } else {
 
             setFirstItemComparative(object1)
             setSecondItemComparative(object2)
         }
 
-    }, [ itemComparative1, object1,object2])
+    }, [itemComparative1, object1, object2])
     return (
         <>
             <Overflow>
-
-            <ContainerPadding>
-                <Title text='mais procuradas' />
-                <Sale />
-            </ContainerPadding>
+                <ContainerPadding>
+                    <Title text='mais procuradas' />
+                    <Sale />
+                </ContainerPadding>
             </Overflow>
+
             <Box sx={{ background: '#F1FCFF' }}>
                 <ContainerPadding>
                     <TitleCentred text='conheça as marcas' />
@@ -106,23 +110,25 @@ export const Home = () => {
             </Box>
 
             <ContainerPadding>
-            <Title text='compare os modelos' />
+                <Title text='compare os modelos' />
 
-            <Sticky top={48}>
-                <Stack sx={{mt:1}} spacing={0} direction='row' justifyContent='space-around'>
-                <SelectItemToCompare setItemComparative={setItemComparative1} id='primeira' raquetes={Data} itemComparative={itemComparative1}  />
-                <SelectItemToCompare setItemComparative={setItemComparative2} id='segunda' raquetes={Data} itemComparative={itemComparative2} />
-                </Stack>
-            </Sticky>
-            <Comparativo object={firstItemComparative} object2={secondItemComparative} />
+                <Sticky top={48}>
+                    <Stack sx={{ mt: 1 }} spacing={0} direction='row' justifyContent='space-around'>
+                        <SelectItemToCompare setItemComparative={setItemComparative1} id='primeira' raquetes={Data} itemComparative={itemComparative1} />
+                        <SelectItemToCompare setItemComparative={setItemComparative2} id='segunda' raquetes={Data} itemComparative={itemComparative2} />
+                    </Stack>
+                </Sticky>
+                <Comparativo object={firstItemComparative} object2={secondItemComparative} />
             </ContainerPadding>
+
             <BannerGlass
                 raquetes={raquetes}
                 logo={logoHeros}
                 btnText={'tecnologia de ponta'}
                 description={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni aut magnam sed velit rerum odio necessitatibus hic numquam molestias dicta.'}
             />
-            <Stack sx={{ height: 60 }}>
+
+            <Stack sx={{ height: 460 }}>
 
             </Stack>
         </>
