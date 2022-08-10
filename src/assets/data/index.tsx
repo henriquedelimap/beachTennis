906247cd6e4966bea42be8d4d5a680a23bd85e63
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-09 21:49:47 -0300

    first commit

--- src/assets/data/index.tsx
+++ src/assets/data/index.tsx
@@ -297,29 +297,5 @@ export const Data = [
         }
     },
 
-    {
-        id: 13,
-        img: [gold22, gold22],
-        title: 'oaaaaaaaa',
-        subtitle: 'Beach Tennis Quicksand GOLD',
-        
-        price: {
-            original: 1.900,
-            sale: 1.499
-        },
-        chips: ['2022', 'Carbon 12K'],
-        year: '2022',
-        stored: 0,
-        description: `Composta por 100% CARBON 12K com o novo BLACK EVA EXTRA SOFT de alta densidade, mais macio, onde a batida da bola na raquete é sentida intensamente, sem deixar de lado o controle e o conforto. Ideal para jogadores de nível intermediário e avançado em Beach Tennis, que buscam um equipamento que gere mais potência em seus golpes. A raquete possui um jateamento rugoso especial que promove uma melhor performance de alguns golpes como smash e saque.​​​`,
-        subDescription: `A raquete QUICKSAND GOLD é o mais novo lançamento da marca, é um modelo irmã da SILVER CLUB. Usa as tecnologias SLIM MOULD 21mm, com uma espessura mais fina que as outras raquetes da linha, MULTI-HOLE com furos espalhados somente ao redor do centro da raquete e ainda o exclusivo e patenteado FUTURA SYSTEM, um dispositivo tecnológico que permite uma redução consistente das vibrações causadas pelo impacto contínuo da bola, amenizando traumas do pulso, de cotovelo e de ombro, ao mesmo tempo em que mantém a raquete no mais alto nível de jogo.`,
-        weight: 335,
-        dimensions: [50, 3, 30],
-        color: ['Dourado', 'Preto'],
-        material: '100% Full Carbon 12K',
-        role: {
-            category: 'ocean air',
-            brand: logoQuickSand
-
-        }
-    },
+    
 ]
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react"
 
 export const ContainerPadding = ({ children} : {children: ReactNode}) => {
     return (
-        <Box sx={{p: { xs: 4, md: 12, lg: 12 }, pt: { xs: 4, md: 4, lg: 4 } }}>
+        <Box sx={{p: { xs: 1, md: 12, lg: 12 }, pt: { xs: 4, md: 4, lg: 4 } }}>
             <Stack spacing={{ xs: 4, md: 4, lg: 4 }} >
                 {children}
             </Stack>
