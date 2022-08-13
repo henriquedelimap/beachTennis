a5af079af8c67ad91d5b50cc34a01f003cfaa378
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-08-13 00:48:53 -0300

    first commit

--- src/Pages/Carrinho/index.tsx
+++ src/Pages/Carrinho/index.tsx
@@ -18,7 +18,7 @@ export const CarrinhoPage = () => {
 
             <Grid item direction='column' container xs={12} md={12} lg={6} sx={{ p: { xs: 1, md: 3.2, lg: 3.2 } }} >
                 <Sticky top={30} index={1000}>
-                    <Title text='preencha seus dados' />
+                    <Title text='dados pessoais' />
                     <FormularioDados />
                 </Sticky>
             </Grid>
